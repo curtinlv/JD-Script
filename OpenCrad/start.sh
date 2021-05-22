c58d49807e937e5fa0dc46f58f04a074cd2750cd
curtinlv <curtinlv@126.com>
2021-05-23 02:07:59 +0800

    新增shtart.sh执行脚本，使用Env环境参数，则ini配置文件参数不会生效。

--- OpenCrad/start.sh
+++ OpenCrad/start.sh
@@ -1,10 +1,10 @@
 #!/bin/env bash
-## 使用Env环境执行方式，ini配置文件参数不会生效。
+## 使用Env环境执行方式，OpenCradConfig.ini配置文件参数不会生效。
 ## 执行命令 ：sh start.sh
 ## 2021.5.22
 ## By Curtin
 
-################### ↓↓↓【以下需要配置的参数，代替了】↓↓↓ ###################
+################### ↓↓↓【以下需要配置的参数，代替配置文件OpenCradConfig.ini】↓↓↓ ###################
 #京东cookie 格式：pt_key=xxx;pt_pin=xxx; & pt_key=xxx;pt_pin=xxx; (多账号&分隔)
 export JD_COOKIE='你的京东Cookie放这里，单引号保留'
 
