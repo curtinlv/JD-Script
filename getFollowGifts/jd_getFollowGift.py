5d8d2a4c527097f2e5170d8e0d0c8e39489cda65
curtinlv <curtinlv@126.com>
2021-06-26 20:22:37 +0800

    适配各种平台ck

--- getFollowGifts/jd_getFollowGift.py
+++ getFollowGifts/jd_getFollowGift.py
@@ -103,30 +103,112 @@ notify_mode = []
 message_info = ''''''
 usergetGiftinfo = {}
 
-def getCookie():
-    global cookies
-    ckfile = pwd + 'JDCookies.txt'
-    try:
-        if os.path.exists(ckfile):
-            with open(ckfile, "r", encoding="utf-8") as f:
-                cks = f.read()
-                f.close()
-            if 'pt_key=' in cks and 'pt_pin=' in cks:
-                r = re.compile(r"pt_key=.*?pt_pin=.*?;", re.M | re.S | re.I)
-                cks = r.findall(cks)
-                if len(cks) > 0:
-                    cookies = ''
-                    for i in cks:
-                        cookies += i
+class getJDCookie(object):
+    # 适配各种平台环境ck
+    def getckfile(self):
+        if os.path.exists('/ql/config/env.sh'):
+            print("当前环境青龙面板新版")
+            return '/ql/config/env.sh'
+        elif os.path.exists('/ql/config/cookie.sh'):
+            print("当前环境青龙面板旧版")
+            return '/ql/config/env.sh'
+        elif os.path.exists('/jd/config/config.sh'):
+            print("当前环境V4")
+            return '/jd/config/config.sh'
+        elif os.path.exists(pwd + 'JDCookies.txt'):
+            return pwd + 'JDCookies.txt'
+    # 获取cookie
+    def getCookie(self):
+        global cookies
+        ckfile = self.getckfile()
+        try:
+            if os.path.exists(ckfile):
+                with open(ckfile, "r", encoding="utf-8") as f:
+                    cks = f.read()
+                    f.close()
+                if 'pt_key=' in cks and 'pt_pin=' in cks:
+                    r = re.compile(r"pt_key=.*?pt_pin=.*?;", re.M | re.S | re.I)
+                    cks = r.findall(cks)
+                    if len(cks) > 0:
+                        cookies = ''
+                        for i in cks:
+                            cookies += i
+            else:
+                with open(ckfile, "w", encoding="utf-8") as f:
+                    cks = "#多账号换行，以下示例：（通过正则获取此文件的ck，理论上可以自定义名字标记ck，也可以随意摆放ck）\n账号1【Curtinlv】cookie1;\n账号2【TopStyle】cookie2;"
+                    f.write(cks)
+                    f.close()
+                pass
+        except Exception as e:
+            print(f"【getCookie Error】{e}")
+
+    # 检测cookie格式是否正确
+    def getUserInfo(self, ck, pinName, userNum):
+        url = 'https://me-api.jd.com/user_new/info/GetJDUserInfoUnion?orgFlag=JD_PinGou_New&callSource=mainorder&channel=4&isHomewhite=0&sceneval=2&sceneval=2&callback=GetJDUserInfoUnion'
+        headers = {
+            'Cookie': ck,
+            'Accept': '*/*',
+            'Connection': 'close',
+            'Referer': 'https://home.m.jd.com/myJd/home.action',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Host': 'me-api.jd.com',
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Mobile/15E148 Safari/604.1',
+            'Accept-Language': 'zh-cn'
+        }
+        try:
+            resp = requests.get(url=url, verify=False, headers=headers, timeout=60).text
+            r = re.compile(r'GetJDUserInfoUnion.*?\((.*?)\)')
+            result = r.findall(resp)
+            userInfo = json.loads(result[0])
+            nickname = userInfo['data']['userInfo']['baseInfo']['nickname']
+            return ck, nickname
+        except Exception:
+            context = f"账号{userNum}【{pinName}】Cookie 已失效！请重新获取。"
+            print(context)
+            return ck, False
+
+    def iscookie(self):
+        """
+        :return: cookiesList,userNameList,pinNameList
+        """
+        cookiesList = []
+        userNameList = []
+        pinNameList = []
+        if 'pt_key=' in cookies and 'pt_pin=' in cookies:
+            r = re.compile(r"pt_key=.*?pt_pin=.*?;", re.M | re.S | re.I)
+            result = r.findall(cookies)
+            if len(result) >= 1:
+                print("您已配置{}个账号".format(len(result)))
+                u = 1
+                for i in result:
+                    r = re.compile(r"pt_pin=(.*?);")
+                    pinName = r.findall(i)
+                    pinName = unquote(pinName[0])
+                    # 获取账号名
+                    ck, nickname = self.getUserInfo(i, pinName, u)
+                    if nickname != False:
+                        cookiesList.append(ck)
+                        userNameList.append(nickname)
+                        pinNameList.append(pinName)
+                    else:
+                        u += 1
+                        continue
+                    u += 1
+                if len(cookiesList) > 0 and len(userNameList) > 0:
+                    return cookiesList, userNameList, pinNameList
+                else:
+                    print("没有可用Cookie，已退出")
+                    exit(3)
+            else:
+                print("cookie 格式错误！...本次操作已退出")
+                exit(4)
         else:
-            with open(ckfile, "w", encoding="utf-8") as f:
-                cks = "#多账号换行，以下示例：\ncookie1\ncookie2"
-                f.write(cks)
-                f.close()
-            pass
-    except Exception as e:
-        print(f"【getCookie Error】{e}")
-getCookie()
+            print("cookie 格式错误！...本次操作已退出")
+            exit(4)
+
+getCk = getJDCookie()
+getCk.getCookie()
+
 #  取ENV环境Ck 设置方法：export JD_COOKIE="你的ck"（多账号&分隔）
 if "JD_COOKIE" in os.environ:
     if len(os.environ["JD_COOKIE"]) > 10:
@@ -397,46 +479,6 @@ def send(title, content):
 
 
 
-def iscookie():
-    """
-    :return: cookiesList,userNameList,pinNameList
-    """
-    isUpdate()
-    cookiesList = []
-    userNameList = []
-    pinNameList = []
-    if 'pt_key=' in cookies and 'pt_pin=' in cookies:
-        r = re.compile(r"pt_key=.*?pt_pin=.*?;", re.M | re.S | re.I)
-        result = r.findall(cookies)
-        if len(result) >= 1:
-            message("您已配置{}个账号".format(len(result)))
-            u = 1
-            for i in result:
-                r = re.compile(r"pt_pin=(.*?);")
-                pinName = r.findall(i)
-                pinName = unquote(pinName[0])
-                # 获取账号名
-                ck, nickname = getUserInfo(i, pinName,u)
-                if nickname != False:
-                    cookiesList.append(ck)
-                    userNameList.append(nickname)
-                    pinNameList.append(pinName)
-                else:
-                    u += 1
-                    continue
-                u += 1
-            if len(cookiesList) > 0 and len(userNameList) > 0:
-                return cookiesList, userNameList, pinNameList
-            else:
-                message("没有可用Cookie，已退出")
-                exitCodeFun(3)
-        else:
-            message("cookie 格式错误！...本次操作已退出")
-            exitCodeFun(4)
-    else:
-        message("cookie 格式错误！...本次操作已退出")
-        exitCodeFun(4)
-
 
 # 检查是否有更新版本
 
@@ -487,30 +529,6 @@ def outfile(filename, context):
         f1.close()
 
 
-def getUserInfo(ck, pinName,userNum):
-    url = 'https://me-api.jd.com/user_new/info/GetJDUserInfoUnion?orgFlag=JD_PinGou_New&callSource=mainorder&channel=4&isHomewhite=0&sceneval=2&sceneval=2&callback=GetJDUserInfoUnion'
-    headers = {
-        'Cookie': ck,
-        'Accept': '*/*',
-        'Connection': 'close',
-        'Referer': 'https://home.m.jd.com/myJd/home.action',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Host': 'me-api.jd.com',
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Mobile/15E148 Safari/604.1',
-        'Accept-Language': 'zh-cn'
-    }
-    try:
-        resp = requests.get(url=url, verify=False, headers=headers, timeout=60).text
-        r = re.compile(r'GetJDUserInfoUnion.*?\((.*?)\)')
-        result = r.findall(resp)
-        userInfo = json.loads(result[0])
-        nickname = userInfo['data']['userInfo']['baseInfo']['nickname']
-        return ck, nickname
-    except Exception:
-        context = f"账号{userNum}【{pinName}】Cookie 已失效！请重新获取。"
-        send(f"【{scriptName}】Cookie 已失效！", context)
-        message(context)
-        return ck, False
 def getRemoteShopid():
     url = base64.decodebytes(
         b"aHR0cHM6Ly9naXRlZS5jb20vY3VydGlubHYvUHVibGljL3Jhdy9tYXN0ZXIvRm9sbG93R2lmdHMvc2hvcGlkLnR4dA==")
@@ -597,7 +615,7 @@ def getGiftresult(result, nickname, pinName, uNum):
 def start():
     print(scriptHeader)
     outfile("Readme.md", readmes)
-    cookiesList, userNameList, pinNameList = iscookie()
+    cookiesList, userNameList, pinNameList = getCk.iscookie()
     userNum = len(cookiesList)
     message(f"有效账号{userNum}个")
     message(f"开始：{scriptName}")
