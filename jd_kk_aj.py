9797a02010c520e169570b16e384a893345bf0ed
curtinlv <curtinlv@126.com>
2021-11-22 15:41:13 +0800

    安佳牛奶 11.1-11.30

--- jd_kk_aj.py
+++ jd_kk_aj.py
@@ -343,10 +343,12 @@ def start():
         print(f"## 用户{a}【{nickname}】")
         if a == 1:
             sleep(1)
-            signUuid = activityContent(header, pin)
             accessLogbody = f'venderId=1000014486&code=46&pin={quote(pin)}&activityId={activityId}&pageUrl=https%3A%2F%2Flzkjdz-isv.isvjcloud.com%2Fpool%2Fcaptain%2F1818505%3FactivityId%3Df88dd152fdc049f3b92aa58339b26345%26signUuid%3D{signUuid}%26shareuserid4minipg%3D{shareuserid4minipg}%26shopid%3D1000014486&subType=app&adSource='
+            signUuid = activityContent(header, pin)
             shareuserid4minipg = pin
             Masternickname = nickname
+            print(f"ck1的助力码：{signUuid}")
+            print(f"shareuserid4minipg：{shareuserid4minipg}")
             print(f"用户{a}[{nickname}]>>助力>>>[Curtinlv]")
             accessLog(header, accessLogbody)
             bindWithVender(ck)
